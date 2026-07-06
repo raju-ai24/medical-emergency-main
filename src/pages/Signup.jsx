e4f3985b990337011cb552ef5483f21171d1d745
rx-dev <rxpineple@gmail.com>
2026-07-06 21:50:53 +0530

    done done

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -50,7 +50,7 @@ const Signup = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/signup", {
+      const response = await fetch("https://medical-emergency-main-backend.onrender.com/api/auth/signup", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
